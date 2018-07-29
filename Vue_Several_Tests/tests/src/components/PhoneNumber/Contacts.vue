66c0b1a51ec8dcfca2e952df3642b38bcb4dfac9
KoreanJungYJ <jung22337@naver.com>
2018-07-29 17:15:05 +0900

    router-link 방식 router.push로 변경

--- Vue_Several_Tests/tests/src/components/PhoneNumber/Contacts.vue
+++ Vue_Several_Tests/tests/src/components/PhoneNumber/Contacts.vue
@@ -3,10 +3,17 @@
     <h1>연락처</h1>
     <div class="wrapper">
       <div v-for="c in contacts" :key="c.no" class="contacts">
-        <!--<router-link :to="`/contacts/${c.no}`">-->
+        <!--<router-link :to="`/contacts/${c.no}`">
         <router-link :to="{ name: 'contact-by-no', params: { no: c.no }}">
+          여기서 name은 router의 index.js에서 설정한 컴포넌트의 이름,
+          params는 넘겨줄 parameters
+          명명된 라우팅 방식
           {{ c.name }}
         </router-link>
+        -->
+        <span style="cursor:pointer" @click="navigate(c.no)">
+          [ {{ c.name }} ]
+        </span>
       </div>
       <router-view></router-view>
     </div>
@@ -23,6 +30,20 @@ export default {
       contacts: contactList.contacts,
     }
   },
+  methods: {
+    navigate (to) {
+      if (confirm('상세 정보를 보시겠습니까?')) {
+        this.$router.push({
+          name: 'contact-by-no',
+          params: { no: to }
+        }, () => {
+          console.log(`/contacts${to} 로 이동 완료`);
+        })
+      } else {
+        alert('취소되었습니다!');
+      }
+    }
+  }
 }
 </script>
 
